3b9f79af9ae5cdb117718255e36b57d85832f472
seanazu <87694301+seanazu@users.noreply.github.com>
2021-09-01 23:48:09 +0300

    Add files via upload

---
+++ ChatApp Messenger/my-app/src/Main/MainComp.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { Switch,Route } from 'react-router-dom';
+import LoginComp from '../Login/LoginComp';
+import SignUpComponent from '../Login/SignUpComponent';
+import MainPage from'./MainPage'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Slide from '@material-ui/core/Slide';
+import PropTypes from 'prop-types';
+
+
+function HideOnScroll(props) {
+    const { children, window } = props;
+    const trigger = useScrollTrigger({ target: window ? window() : undefined });
+    
+  
+    return (
+      <Slide appear={false} direction="down" in={!trigger}>
+        {children}
+      </Slide>
+    );
+  }
+  
+  HideOnScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    window: PropTypes.func,
+  };
+
+
+const MainComp = () => {
+    
+    return (
+        <div>
+             <React.Fragment>
+              <CssBaseline />
+              <HideOnScroll >
+                <AppBar style={{height:'130px',backgroundColor:'skyblue'}}>
+                  <Toolbar>
+                      <div style={{margin:'44%'}}>
+                  <img src="https://api.freelogodesign.org/files/463246c69261474caeb6025cc71ced07/thumb/logo_200x200.png?v=637617581860000000" alt="Subscription Web Site" style={{height:'200px', width:'200px',margin:'auto'}}/>
+                  </div>
+                  </Toolbar>
+                </AppBar>
+              </HideOnScroll>
+              <Toolbar />
+            </React.Fragment>
+
+            <Switch>
+                <Route exact path='/' component={LoginComp} />
+                <Route path='/signUpComp' component={SignUpComponent} />
+                <Route path='/mainpage/:id' component={MainPage} />
+            </Switch>
+
+        </div>
+    );
+};
+
+export default MainComp;
---
+++ ChatApp Messenger/my-app/src/Main/MainPage.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react';
+import {Switch, Route, Link } from 'react-router-dom';
+import UsersComp from '../Users/UsersComp'
+import './MainComp.css'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Paper from '@material-ui/core/Paper';
+import history from '../history';
+import GroupsComp from '../Groups/GroupsComp';
+import ChatBoxComp from '../Private Chats/ChatBoxComp'
+import UserInfoComp from '../User Info/UserInfoComp';
+import CreateGroupComp from '../Groups/CreateGroupComp';
+import GroupChatComp from '../Groups/GroupChatComp';
+
+
+
+
+const MainPage = (props) => {
+  const [value, setValue] = React.useState(2);
+
+  useEffect(()=>{
+   let id = props.match.params.id
+   if(id == false){
+     history.push('/')
+   }
+    
+  },[])
+
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+    return (
+        <div >
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+              <Paper square style={{margin:'auto' , width:'640px'}}>
+              <Tabs
+                value={value}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={handleChange}
+                aria-label="disabled tabs example"
+               
+              >
+                <Tab label="Private Chats" onClick={()=> history.push('/mainpage/usersComp')} />
+                <Tab label="Groups" onClick={()=> history.push('/mainpage/groupsComp')}  />
+                <Tab label="My Info" onClick={()=> history.push('/mainpage/userInfo')}/>
+                <Tab label="Log Out" onClick={()=> history.push('/')}/>
+
+              </Tabs>
+            </Paper>
+
+
+            <Switch>
+                <Route path= "/mainpage/usersComp" component={UsersComp} />
+                <Route path= "/mainpage/groupsComp" component={GroupsComp} />
+                <Route path= "/mainpage/groupChatComp/:room/:userId/:username" component={GroupChatComp} />
+                <Route path= "/mainpage/createGroup" component={CreateGroupComp} />
+                <Route path= "/mainpage/chatBoxComp/:room/:userId/:username" component={ChatBoxComp} />
+                <Route path= "/mainpage/userInfo" component={UserInfoComp} />
+
+            </Switch>
+        </div>
+    );
+};
+
+export default MainPage;
