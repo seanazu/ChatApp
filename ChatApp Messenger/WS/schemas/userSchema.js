c3a864784faa7c405dee3426ac6fb231859a2214
seanazu <87694301+seanazu@users.noreply.github.com>
2021-09-02 23:29:56 +0300

    Update userSchema.js

--- ChatApp Messenger/WS/schemas/userSchema.js
+++ ChatApp Messenger/WS/schemas/userSchema.js
@@ -9,19 +9,7 @@ const userSchema = new appSchema({
     age : String ,
     city : String ,
     status : String ,
-    image : String ,
-    groups :[
-        {
-            _id: false,
-            groupId : String
-        }
-    ],
-    duoChats : [
-        {
-            _id: false,
-            chatId: String
-        }
-    ] , 
+    image : String , 
     block : [
         {
             _id: false,
