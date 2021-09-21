95b4b8a53ff34a8fc0e501a6951cb8f7847b9aff
seanazu <87694301+seanazu@users.noreply.github.com>
2021-09-21 23:55:08 +0300

    Update EditGroupComp.js

--- ChatApp Messenger/my-app/src/Groups/EditGroupComp.js
+++ ChatApp Messenger/my-app/src/Groups/EditGroupComp.js
@@ -1,4 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState , useRef} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import axios from 'axios';
+import { Card, Checkbox } from '@material-ui/core';
+import history from '../history';
+import _uniqueId from 'lodash.uniqueid';
+
+const useStyles1 = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  
+
 
 
 const EditGroupComp = () => {
