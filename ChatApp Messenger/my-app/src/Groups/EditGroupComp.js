aa571ad9a5e41c1bb89ed3d962de0c5bf1e1ea97
seanazu <87694301+seanazu@users.noreply.github.com>
2021-09-21 23:54:37 +0300

    Update EditGroupComp.js

--- ChatApp Messenger/my-app/src/Groups/EditGroupComp.js
+++ ChatApp Messenger/my-app/src/Groups/EditGroupComp.js
@@ -4,6 +4,61 @@ import React from 'react';
 const EditGroupComp = () => {
     return (
         <div>
+             <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes1.paper}>
+              <AddCircleOutlineIcon color='secondary' style={{fontSize:'xx-large'}}/>
+              <Typography component="h1" variant="h5">
+              Create Group
+              </Typography>
+              <form className={classes1.form} noValidate>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      label="Group Name"
+                      autoComplete="Full Name"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      label="Status"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      variant="outlined"
+                      required
+                      fullWidth
+                      label="Image"
+                      autoComplete="current-password"
+                      
+                    />
+                  </Grid>
+                </Grid>
+                <br/>
+                <Typography>
+                    Group Members: 
+                </Typography>
+
+                <br/>
+                <Grid >
+                  <Grid item>
+                  <Button variant="contained" color="primary"  > Save</Button>{' '}
+                  <Button variant="contained" color="primary"  > Cancel</Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </div>
+            <Box mt={5}>
+            </Box>
+          </Container>
+            
             
         </div>
     );
