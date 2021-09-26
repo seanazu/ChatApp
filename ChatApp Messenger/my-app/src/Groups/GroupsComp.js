8d6be7f107988dca8cefdf86a073c456848b0345
seanazu <87694301+seanazu@users.noreply.github.com>
2021-09-26 23:51:41 +0300

    Update GroupsComp.js

--- ChatApp Messenger/my-app/src/Groups/GroupsComp.js
+++ ChatApp Messenger/my-app/src/Groups/GroupsComp.js
@@ -16,7 +16,9 @@ const useStyles = makeStyles({
         maxWidth: 450,
         margin:'auto',
         width:'450px', 
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+        display:'flex',
+        justifyContent:'space-between'
       },
       media: {
         height: 140,
