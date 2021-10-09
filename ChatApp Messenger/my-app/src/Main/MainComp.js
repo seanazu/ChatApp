676a1885397bbbceb67a252767abe891a7b2ab21
seanazu <87694301+seanazu@users.noreply.github.com>
2021-10-09 18:47:07 +0300

    Update MainComp.js

--- ChatApp Messenger/my-app/src/Main/MainComp.js
+++ ChatApp Messenger/my-app/src/Main/MainComp.js
@@ -1,4 +1,4 @@
-import React, from 'react';
+import React from 'react';
 import { Switch,Route } from 'react-router-dom';
 import LoginComp from '../Login/LoginComp';
 import SignUpComponent from '../Login/SignUpComponent';
